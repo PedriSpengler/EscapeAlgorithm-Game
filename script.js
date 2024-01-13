12971a8af2bc4d02306bd10c3657321f88ebeb47
PedriSpengler <pedroj.oficial@gmail.com>
2024-01-12 22:31:40 -0400

    Final Game Screen publish

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*               MENU               */
+
 function open_page(id) {
     var element = document.getElementById(id);
 
@@ -15,7 +17,52 @@ function close_page(id) {
     var element = document.getElementById(id);
 
     if (element) {
-        element.classList.add("hidden"); // Use "hidden" em vez de "remove"
-        element.style.top = "-110vh"; // Adicionando a unidade de medida correta
+        element.classList.add("hidden");
+        element.style.top = "-110vh"; 
+    }
+}
+
+
+/*            TRANSIÇÃO MENU -> GAME           */
+
+function open_game(id){
+    var element = document.getElementById(id);
+
+    if(element){
+        element.classList.add("open");
+
+        if(id === "game"){
+            element.style.top = "0";
+            element.classList.remove('hidden');
+        }
+    }
+}
+
+
+
+
+
+
+
+/*                GAME               */
+
+
+function runAction(id) {
+    var originalButton = document.getElementById(id);
+    var stopButton = document.getElementById('stop');
+
+    if (originalButton && stopButton) {
+        originalButton.style.display = 'none';
+        stopButton.style.display = 'flex';
+    }
+}
+
+function stopAction(id) {
+    var stopButton = document.getElementById(id);
+    var originalButton = document.getElementById('run');
+
+    if (originalButton && stopButton) {
+        stopButton.style.display = 'none';
+        originalButton.style.display = 'flex';
     }
 }
